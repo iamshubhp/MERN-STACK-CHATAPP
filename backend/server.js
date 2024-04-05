3a6c4380a1ef6d06f456b5dd21c33547991e4c5d
iamshubhp <shubhpatel1812002@gmail.com>
2024-04-05 13:00:12 -0400

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+const cors = require("cors");
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -18,6 +19,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
